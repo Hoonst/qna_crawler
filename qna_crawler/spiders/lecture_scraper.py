a3c1a71cd6378ab03b0b1050aff5824c3c3abe5b
hoonst <yoonhs1993@nate.com>
2020-02-28 19:37:54 +0900

    2020202020

--- qna_crawler/spiders/lecture_scraper.py
+++ qna_crawler/spiders/lecture_scraper.py
@@ -16,10 +16,11 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+import chromedriver_binary
 
 ua = UserAgent()
 co = webdriver.ChromeOptions()
-co.add_argument('/Users/yoonhoonsang/Desktop/internet_lecture/qna_crawler/chromedriver')
+
 co.add_argument("log-level=3")
 co.add_argument("headless")
 co.add_argument('user-agent=%s'.format(ua.random))
@@ -27,7 +28,7 @@ co.add_argument("lang=ko_KR")
 
 
 def get_proxies(co=co):
-    driver = webdriver.Chrome(chrome_options=co)
+    driver = webdriver.Chrome(executable_path='/home/yoonhs19933/qna_crawler/chromedriver', chrome_options=co)
     driver.get("https://free-proxy-list.net/")
 
     PROXIES = []
@@ -58,7 +59,7 @@ def proxy_driver(PROXIES, co=co):
     capabilities = webdriver.DesiredCapabilities.CHROME
     prox.add_to_capabilities(capabilities)
 
-    driver = webdriver.Chrome(chrome_options=co, desired_capabilities=capabilities)
+    driver = webdriver.Chrome(executable_path='~/qna_crawler/chromedriver', chrome_options=co, desired_capabilities=capabilities)
 
     return driver
 
@@ -92,13 +93,13 @@ class ETOOSSpider(scrapy.Spider):
 
         while running:
             try:
-                co.add_argument('user-agent=%s'.format(ua.random))
-                self.browser = webdriver.Chrome(chrome_options = co)
+                co.add_argument(f'user-agent={ua.random}')
+                self.browser = webdriver.Chrome(executable_path='/home/yoonhs19933/qna_crawler/chromedriver', chrome_options = co)
 
-                base_url = 'https://www.etoos.com/teacher/board/sub04_math/board_list.asp?teacher_id=200386&selSearchType=&txtSearchWD=&BOARD_ID=2007&QUST_TYPE_CD=&GOOD_QUST_YN=&MOV_YN=&MEM_YN=&NTView=&page=%s'.format(str(page))
+                base_url = f'https://www.etoos.com/teacher/board/sub04_math/board_list.asp?teacher_id=200386&selSearchType=&txtSearchWD=&BOARD_ID=2007&QUST_TYPE_CD=&GOOD_QUST_YN=&MOV_YN=&MEM_YN=&NTView=&page={str(page)}'
 
                 self.browser.get(base_url)
-                print('Online to %s'.format(base_url))
+                print(f'Online to {base_url}')
 
                 title = WebDriverWait(self.browser, 10) \
                                         .until(EC.presence_of_element_located((By.CSS_SELECTOR, "table.subcomm_tbl_board")))
@@ -179,7 +180,7 @@ class MegaSpider(scrapy.Spider):
         while running:
             try:
                 co.add_argument('user-agent=%s'.format(ua.random))
-                self.browser = webdriver.Chrome(chrome_options = co)
+                self.browser = webdriver.Chrome(executable_path='/home/yoonhs19933/qna_crawler/chromedriver', chrome_options = co)
 #                 print(f'Current Page {page}')
 
                 base_url = 'http://www.megastudy.net/teacher_v2/bbs/bbs_list_ax.asp?tec_cd=rimbaud666&tec_nm=%uC870%uC815%uC2DD&tec_type=1&brd_cd=784&brd_tbl=MS_BRD_TEC784&brd_kbn=qnabbs&dom_cd=5&LeftMenuCd=3&LeftSubCd=1&HomeCd=134&page=%s&chr_cd=&sub_nm=&ans_yn=&smode=1&sword=&TmpFlg=0.24915805251066403'.format(str(page))
@@ -264,7 +265,7 @@ class SkySpider(scrapy.Spider):
         while running:
             try:
                 co.add_argument('user-agent=%s'.format(ua.random))
-                self.browser = webdriver.Chrome(chrome_options = co)
+                self.browser = webdriver.Chrome(executable_path='/home/yoonhs19933/qna_crawler/chromedriver', chrome_options = co)
 
                 base_url = 'https://skyedu.conects.com/teachers/teacher_qna/?t_id=jhc01&cat1=1&page=%s'.format(str(page))
 
@@ -345,7 +346,7 @@ class MiMacSpider(scrapy.Spider):
         while running:
             try:
                 co.add_argument('user-agent=%s'.format(ua.random))
-                self.browser = webdriver.Chrome(chrome_options = co)
+                self.browser = webdriver.Chrome(executable_path='/home/yoonhs19933/qna_crawler/chromedriver', chrome_options = co)
 
                 base_url = 'http://www.mimacstudy.com/tcher/studyQna/getStudyQnaList.ds?tcd=531&currPage=%s'.format(str(page))
 
